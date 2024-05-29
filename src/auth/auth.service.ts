2e66ebe4f6b725799be75d9e0f5f109604e1843d
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-05-29 14:18:42 -0300

    feat(auth): local strategy validation using guards

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if (user) {
+            // Check if password compares to hash correctly
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (isPasswordValid) {
+                return {
+                    ...user,
+                    password: undefined,
+                };
+            }
+        }
+
+        throw new Error('Email address or password provided is incorrect.');
+    }
+
+}
